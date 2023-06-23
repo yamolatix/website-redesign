a99f20424ecb31de382fbe0b1ce7c8118a7e4dfc
Yamila Ortiz Toscano <yamilaortiztoscano@gmail.com>
2023-06-23 18:03:28 -0300

    responsive

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var menu = document.getElementById('menu');
+function toggleMenu() {
+    menu.classList.toggle('hidden');
+    menu.classList.toggle('w-full');
+    menu.classList.toggle('h-screen');
+}
